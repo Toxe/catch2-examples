890240b03a76d8d56ea8e648fc7a3e075fd87954
Toxe <toxe@toxe.net>
2021-09-25 09:03:40 +0200

    Format code

--- src/single_header_multiple_test_files/sub.test.cpp
+++ src/single_header_multiple_test_files/sub.test.cpp
@@ -5,10 +5,10 @@
 
 TEST_CASE("sub")
 {
-    REQUIRE(sub(0, 0) ==  0);
-    REQUIRE(sub(1, 0) ==  1);
+    REQUIRE(sub(0, 0) == 0);
+    REQUIRE(sub(1, 0) == 1);
     REQUIRE(sub(0, 1) == -1);
-    REQUIRE(sub(1, 1) ==  0);
+    REQUIRE(sub(1, 1) == 0);
     REQUIRE(sub(2, 3) == -1);
-    REQUIRE(sub(3, 2) ==  1);
+    REQUIRE(sub(3, 2) == 1);
 }
