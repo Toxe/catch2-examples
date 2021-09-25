3fa64eea080548c83b1694a96e679eabb69b95f0
Toxe <toxe@toxe.net>
2021-09-25 08:47:18 +0200

    Call every libtestee function

--- src/single_header_multiple_test_files/main.cpp
+++ src/single_header_multiple_test_files/main.cpp
@@ -1,8 +1,21 @@
+#include <iostream>
+
 #include "testee.hpp"
 
 int main()
 {
-    int a = add(2, 3);
+    int a = add(3, 2);
+    int m = mul(3, 2);
+    int s = sub(3, 2);
 
-    return a == 5 ? 0 : 1;
+    if (a != 5)
+        return 1;
+
+    if (m != 6)
+        return 1;
+
+    if (s != 1)
+        return 1;
+
+    std::cout << "Everything seems to be working.\n";
 }
